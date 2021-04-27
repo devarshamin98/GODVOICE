2e8fcf5d844732605ea849b926dc84ae94caa0cf
devarshamin98 <79574249+devarshamin98@users.noreply.github.com>
2021-04-27 14:52:52 -0500

    Add files via upload

---
+++ ML-modeling/feature_selection.py
@@ -0,0 +1,93 @@
+## helper functions that datasets with a reduced
+## number of features. Common feature selection
+## methods from sklearn are included.
+
+
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectFromModel,SelectKBest,chi2
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+
+def lasso_feature_selection(X,y,penalty=0.01,model='svm',threshold=0.3):
+    """Function that returns dataset with L1 feature selection applied.
+
+    Args:
+        X -- training dataset for inputs
+        y -- training datsets for outputs
+        penalty -- L1 regularization penalty
+        model -- chosen linear model for applying L1 regularization
+        threshold -- if parameter has a weight lower than 0.3, delete it
+    Returns:
+        X_transformed,clf (tuple) -- new dataset with features selected, along
+        with object that transforms the data
+    """
+    if model == 'svm':
+        lasso_model = LinearSVC(penalty='l1',C=penalty,dual=False)
+    else:
+        # model is logistic regression
+        lasso_model = LogisticRegression(penalty='l1',C=penalty)
+
+    clf = SelectFromModel(lasso_model,threshold=threshold)
+    clf.fit(X,y)
+
+    # transform data using L1 regularization
+    X_transformed = clf.transform(X)
+    return (X_transformed,clf)
+
+def tree_based_feature_selection(X,y,n_estimators=50):
+    """Function that returns dataset with tree-based feature selection applied
+
+    Args:
+        X -- training dataset for inputs
+        y -- training dataset for outputs
+        n_estimators -- number of trees used in the feature selector
+
+    Returns:
+        X_transformed,model (tuple) -- new dataset with features selected, along
+        with object that transforms the data
+    """
+    tree_model = ExtraTreesClassifier(n_estimators=n_estimators)
+    tree_model.fit(X,y)
+    model = SelectFromModel(tree_model,prefit=True)
+    X_transformed = model.transform(X)
+    return (X_transformed,model)
+
+def chi_squared_feature_selection(X,y,k=80):
+    """Function that returns dataset with chi-squared based feature selection
+
+    Args:
+        X -- training dataset for inputs
+        y -- training dataset for outputs
+        k -- number of features to select
+
+    Returns:
+        X_transformed,model (tuple) -- new dataset with features selected, along
+        with object that transforms the data
+    """
+
+    # need to min max scale data first because Chi2 doesn't take negative values
+    min_max_scaler = MinMaxScaler()
+    X_scaled = min_max_scaler.fit_transform(X)
+    transformer = SelectKBest(chi2,k=k)
+    X_transformed = transformer.fit_transform(X_scaled,y)
+    return (X_transformed,transformer)
+
+def pca_feature_selection(X,k=80):
+    """Function that returns dataset k principal components selected
+
+    Args:
+        X -- training dataset for inputs
+        k -- number of features to select
+
+    Returns:
+        X_transformed,pca (tuple) -- new dataset with features selected, along
+        with object that transforms the data
+    """
+    pca = PCA(n_components=k)
+    pca.fit(X)
+    # takes k principal components
+    X_transformed = pca.transform(X)
+    return (X_transformed,pca)
---
+++ ML-modeling/readme.md
@@ -0,0 +1,53 @@
+# Modeling
+
+1. [Preprocessing](#prep)
+2. [Feature Selection](#feat_select)
+3. [Model Selection](#model_select)
+4. [Results](#res)
+
+<a name = "prep"></a>
+## 1. Preprocessing
+### Standard Scaler
+Since many of our features are on different scales, using a feature scaler is
+essential to any machine learning algorithm that uses gradient descent. When
+features are on the same scale, gradient descent **tends** to reach a minimum
+of the cost function faster, and a minimum that results in better model
+predictions.
+
+<a name = "feat_select"></a>
+## 2. Feature Selection
+A key part of this project is feature selection because we start out with 170
+features. The scikit-learn library provides us easy ways of doing this.
+### L1 Feature Selection
+Using L1 regularization is a common way of selecting features in a machine learning
+model. Adding the L1 norm to the cost function of any linear model penalizes less
+important features. So, we create a logistic regression model with an L1 penalty,
+and remove any features that have parameter values than 0.3. We also tried out
+a support vector machine, but it failed to converge (recall that the support
+vector machine is an optimization problem, and so it can fail to converge at a
+minimum).
+### Tree-based Feature Selection
+We can get feature importances by using a random forest model. We create a
+random forest of 50 trees and rank features that tend to appear closer to the
+tree as higher, because this generally means that the feature provides a more
+meaningful split in the data.
+### Chi Squared Feature Selection
+Coming soon.
+### PCA Feature Selection
+Finally, PCA is a very common dimensionality reduction technique that finds
+n principal components, which means that we return the features that account
+for the most variability in the data.
+
+We tested these feature selection methods on the baseline model and got the best
+results with tree-based feature selection, so we chose to proceed with this
+feature selection approach when moving over to the more complex model.
+<a name = "model_select"></a>
+## 3. Model Selection
+### Architecture Development
+### Hyperparameter Tuning
+Coming soon.
+
+<a name = "res"></a>
+## 4. Results
+### Performance metrics
+Coming soon.
